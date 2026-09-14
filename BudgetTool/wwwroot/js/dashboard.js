f19ac7fedfcbbf516f69f2483464495cdc096682
Rinus Luijk <rinusaluijk@gmail.com>
2026-09-14 23:32:33 +0200

    Updated transactions dashboard. Added functions to get stats

--- BudgetTool/wwwroot/js/dashboard.js
+++ BudgetTool/wwwroot/js/dashboard.js
@@ -9,7 +9,6 @@ window.onload = async function () {
 
     accountTransactionData = await loadTransactionItems(accountId, [1]);
     updateBudgetRowValues();
-    console.log("accountTransactionData", accountTransactionData);
     hideSpinner();
 }
 
@@ -45,8 +44,59 @@ function updateBudgetRowValues() {
     const transactionsTotalDiv = document.getElementById("spent");
     transactionsTotalDiv.innerText = currencyFormatter.format(transactionsTotal);
 
-    const remaining = document.getElementById("remaining");
-    remaining.innerText = currencyFormatter.format(budgetTotal - transactionsTotal);
+    const remainingText = document.getElementById("remainingText");
+    const remainingDiv = document.getElementById("remaining");
     const percentageRemaining = document.getElementById("percentageRemaining");
-    percentageRemaining.innerText = `${(100 - (transactionsTotal * 100 / budgetTotal)).toFixed(2)}% of budget`;
+    let remainingAmount = budgetTotal - transactionsTotal;
+    if (remainingAmount >= 0) {
+        remainingText.innerText = "Remaining";
+        remainingDiv.innerText = currencyFormatter.format(remainingAmount);
+        percentageRemaining.innerText = `${(100 - (transactionsTotal * 100 / budgetTotal)).toFixed(2)}% of budget`;
+    } else {
+        remainingText.innerText = "Over";
+        remainingDiv.innerText = currencyFormatter.format(Math.abs(remainingAmount));
+        percentageRemaining.innerText = `${Math.abs((remainingAmount * 100 / budgetTotal)).toFixed(2)}% of budget`;
+    }
+
+    const biggestOverspend = getBiggestOverspend();
+    const over = document.getElementById("biggestOverspendCategory");
+    over.innerText = biggestOverspend.category;
+    const percentageOver = document.getElementById("overSpendAmount");
+    percentageOver.innerText = currencyFormatter.format(biggestOverspend.amount);
+}
+
+function flattenTransactionItems() {
+    let flattenedTransactions = [];
+    let itemIndex = {};
+    for (const [key, value] of accountTransactionData) {
+        value.forEach(item => {
+            itemIndex = flattenedTransactions.findIndex(transaction => transaction.categoryId === item.categoryId);
+            if (itemIndex !== -1) {
+                flattenedTransactions[itemIndex].amount = item.amount;
+            } else {
+                flattenedTransactions.push(item);
+            }
+        })
+    }
+    return flattenedTransactions;
+}
+
+function getBiggestOverspend() {
+    let flattenedTransactionItems = flattenTransactionItems();
+    let biggestOverspend = {"category": "", "amount": 0};
+    let transactionIndex;
+    let currentTransactionItem;
+    let difference;
+    budgetItems.forEach(budgetItem => {
+        transactionIndex = flattenedTransactionItems.findIndex(transactionItem => transactionItem.categoryId === budgetItem.categoryId);
+        if (transactionIndex > -1) {
+            currentTransactionItem = flattenedTransactionItems[transactionIndex];
+            difference = currentTransactionItem.amount - budgetItem.budgetItemAmount;
+            if (difference > biggestOverspend.amount) {
+                biggestOverspend.category = currentTransactionItem.categoryLabel;
+                biggestOverspend.amount = difference;
+            }
+        }
+    });
+    return biggestOverspend;
 }
